49318cded02104819831b95c79009cf4261188c5
Joshua Cleetus <joshua.cleetus@gmail.com>
2025-07-26 14:27:49 -0400

    Initial Commit

---
+++ FlipRecall/FlipRecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipRecallApp.swift
+//  FlipRecall
+//
+//  Created by Joshua Cleetus on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FlipRecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
